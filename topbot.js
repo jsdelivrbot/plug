08571b02b513d95439dd257e616e0751d71f9e54
Samnick86 <jurikj86@gmail.com>
2016-11-29 11:56:34 +0100

    Update topbot.js

--- topbot.js
+++ topbot.js
@@ -236,7 +236,7 @@
     var botCreatorIDs = ["3851534", "4105209"];
 
     var basicBot = {
-        version: "2.9.1",
+        version: "3.0.1",
         status: false,
         name: "basicBot",
         loggedInID: null,
